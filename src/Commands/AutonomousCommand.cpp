b0b3a20e074941058d0b06b775c1a5c818a5f48b
Colemw0 <cole-m-w@sbcglobal.net>
2017-01-24 19:25:56 -0500

    clean code
    rpm and throttle method

--- src/Commands/AutonomousCommand.cpp
+++ src/Commands/AutonomousCommand.cpp
@@ -25,7 +25,7 @@ AutonomousCommand::AutonomousCommand(): Command() {
 
 // Called just before this Command runs the first time
 void AutonomousCommand::Initialize() {
-	Robot::shooterSubsystem->TurnOn();
+	Robot::shooterSubsystem->TurnOnRPM();
 }
 
 // Called repeatedly when this Command is scheduled to run
